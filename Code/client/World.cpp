4491f2ebb9ca707462a794d8825572594d58d4ea
rfortier <rwf@ourchateau.net>
2024-01-31 18:06:33 -0500

    Support full conditional compile of MODDED_BEHAVIOR_COMPATIBILITY
    Solve player 1st person woes
    
    Lighten intrusion into STR main body (References.cpp)
    
    Fixed sorting of AnimationGraphDescriptor behavior variables, this will help when Pandora is more widely adopted (and stops reordering behavior variables, maybe we can get to mod order independence).
    
    Added failList of modded behavior lookups that fail, so we don't repeat expensive failed calls. Even some built-in ones can fail, like wisps.
    
    Deleted dead code, marked some I'm not sure about for deletion.
    
    Update README.md with a decent description of the content and goals of the fork.
    
    Cleanups, comments about how it works.

--- Code/client/World.cpp
+++ Code/client/World.cpp
@@ -25,8 +25,9 @@
 #include <Events/PreUpdateEvent.h>
 #include <Events/UpdateEvent.h>
 
-// MOD BEHAVIORS: add modded behaviors
-#include <ModCompat/BehaviorVar.h>   
+#ifdef MODDED_BEHAVIOR_COMPATIBILITY
+#include <ModCompat/BehaviorVar.h>  
+#endif MODDED_BEHAVIOR_COMPATIBILITY
 
 World::World()
     : m_runner(m_dispatcher)
@@ -57,8 +58,9 @@ World::World()
     ctx().emplace<MapService>(*this, m_dispatcher, m_transport);
     ctx().emplace<KeybindService>(m_dispatcher, ctx().at<InputService>(), ctx().at<DebugService>());
 
+#ifdef MODDED_BEHAVIOR_COMPATIBILITY
     BehaviorVar::Get()->Init();
-    // MOD BEHAVIORS: add modded behaviors
+#endif MODDED_BEHAVIOR_COMPATIBILITY
 }
 
 World::~World() = default;
