rfortier/TiltedEvolution

commit c47c3bef09540106d71db22d427b8aa57c818ecf

Magic effect removal code
FT doesn't build but this allows the function that explicitly removes magic effects to sync when applied to a player

Code/client/Games/References.cpp
@@ -857,6 +857,20 @@ void Actor::StopCombat() noexcept
     s_pStopCombat(this);
 }
 
+#if TP_SKYRIM64
+void Actor::RemoveSpell(MagicItem* apSpell) noexcept
+{
+    TP_THIS_FUNCTION(TRemoveSpell, void, Actor, MagicItem*);
+    POINTER_SKYRIMSE(TRemoveSpell, removeSpell, 38717);
+    if (!apSpell)
+    {
+        spdlog::error("Actor::RemoveSpell: apSpell is null");
+        return;
+    }
+    //spdlog::info("Removing spell: {} from actor: {}", apSpell->formID, formID);
+    TiltedPhoques::ThisCall(removeSpell, this, apSpell);
+}
+#endif
 bool Actor::HasPerk(uint32_t aPerkFormId) const noexcept
 {
     return GetPerkRank(aPerkFormId) != 0;

Code/client/Games/Skyrim/Actor.h
@@ -245,6 +245,7 @@ struct Actor : TESObjectREFR
     void SetCombatTargetEx(Actor* apTarget) noexcept;
     void StartCombat(Actor* apTarget) noexcept;
     void StopCombat() noexcept;
+    void RemoveSpell(MagicItem* apSpell) noexcept;
 
     enum ActorFlags
     {

Code/client/Events/RemoveSpellEvent.h
@@ -1,0 +1,9 @@
+#pragma once
+
+struct RemoveSpellEvent
+{
+    RemoveSpellEvent() = default;
+
+    uint32_t TargetId{};
+    uint32_t SpellId{};
+};

Code/client/Games/Skyrim/Magic/MagicTarget.cpp
@@ -17,7 +17,10 @@
 
 #include <Effects/ActiveEffect.h>
 
+#include <Events/RemoveSpellEvent.h>
+
 TP_THIS_FUNCTION(TAddTarget, bool, MagicTarget, MagicTarget::AddTargetData& arData);
+TP_THIS_FUNCTION(TRemoveSpell, bool, Actor, MagicItem* apSpell);
 TP_THIS_FUNCTION(TCheckAddEffectTargetData, bool, MagicTarget::AddTargetData, void* arArgs, float afResistance);
 TP_THIS_FUNCTION(TFindTargets, bool, MagicCaster, float afEffectivenessMult, int32_t* aruiTargetCount,
                  TESBoundObject* apSource, char abLoadCast, char abAdjust);
@@ -26,6 +29,7 @@ TP_THIS_FUNCTION(THasPerk, bool, Actor, TESForm* apPerk, void* apUnk1, double* a
 TP_THIS_FUNCTION(TGetPerkRank, uint8_t, Actor, TESForm* apPerk);
 
 static TAddTarget* RealAddTarget = nullptr;
+static TRemoveSpell* RealRemoveSpell = nullptr;
 static TCheckAddEffectTargetData* RealCheckAddEffectTargetData = nullptr;
 static TFindTargets* RealFindTargets = nullptr;
 static TAdjustForPerks* RealAdjustForPerks = nullptr;
@@ -173,6 +177,26 @@ bool TP_MAKE_THISCALL(HookAddTarget, MagicTarget, MagicTarget::AddTargetData& ar
     }
 }
 
+// Designed to run when we hook removespell (AddressLib ID is 38717)
+// Sends a message to the server to remove the spell from this player on other clients
+bool TP_MAKE_THISCALL(HookRemoveSpell, Actor, MagicItem* apSpell)
+{   
+    bool result = TiltedPhoques::ThisCall(RealRemoveSpell, apThis, apSpell);
+    if (apThis->GetExtension()->IsLocalPlayer() && result)
+    {
+        // Log spell info
+        //spdlog::info("Removing spell {}, ID: {} from local player", apSpell->GetName() , apSpell->formID);
+        RemoveSpellEvent removalEvent;
+
+        removalEvent.TargetId = apThis->formID;
+        removalEvent.SpellId = apSpell->formID;
+        World::Get().GetRunner().Trigger(removalEvent);
+    }
+
+    return result;
+}
+
+
 bool TP_MAKE_THISCALL(HookCheckAddEffectTargetData, MagicTarget::AddTargetData, void* arArgs, float afResistance)
 {
     if (s_autoSucceedEffectCheck)
@@ -219,6 +243,7 @@ uint8_t TP_MAKE_THISCALL(HookGetPerkRank, Actor, TESForm* apPerk)
 
 static TiltedPhoques::Initializer s_magicTargetHooks([]() {
     POINTER_SKYRIMSE(TAddTarget, addTarget, 34526);
+    POINTER_SKYRIMSE(TRemoveSpell, removeSpell, 38717);
     POINTER_SKYRIMSE(TCheckAddEffectTargetData, checkAddEffectTargetData, 34525);
     POINTER_SKYRIMSE(TFindTargets, findTargets, 34410);
     POINTER_SKYRIMSE(TAdjustForPerks, adjustForPerks, 34053);
@@ -226,6 +251,7 @@ static TiltedPhoques::Initializer s_magicTargetHooks([]() {
     POINTER_SKYRIMSE(TGetPerkRank, getPerkRank, 37698);
 
     RealAddTarget = addTarget.Get();
+    RealRemoveSpell = removeSpell.Get();
     RealCheckAddEffectTargetData = checkAddEffectTargetData.Get();
     RealFindTargets = findTargets.Get();
     RealAdjustForPerks = adjustForPerks.Get();
@@ -233,6 +259,7 @@ static TiltedPhoques::Initializer s_magicTargetHooks([]() {
     RealGetPerkRank = getPerkRank.Get();
 
     TP_HOOK(&RealAddTarget, HookAddTarget);
+    TP_HOOK(&RealRemoveSpell, HookRemoveSpell);
     TP_HOOK(&RealCheckAddEffectTargetData, HookCheckAddEffectTargetData);
     TP_HOOK(&RealFindTargets, HookFindTargets);
     TP_HOOK(&RealAdjustForPerks, HookAdjustForPerks);

Code/client/Services/Generic/MagicService.cpp
@@ -6,6 +6,9 @@
 #include <Events/SpellCastEvent.h>
 #include <Events/InterruptCastEvent.h>
 #include <Events/AddTargetEvent.h>
+#include <Events/RemoveSpellEvent.h>
+
+#include <Messages/RemoveSpellRequest.h>
 
 #include <Messages/SpellCastRequest.h>
 #include <Messages/InterruptCastRequest.h>
@@ -46,6 +49,8 @@ MagicService::MagicService(World& aWorld, entt::dispatcher& aDispatcher, Transpo
     m_notifyInterruptCastConnection = m_dispatcher.sink<NotifyInterruptCast>().connect<&MagicService::OnNotifyInterruptCast>(this);
     m_addTargetEventConnection = m_dispatcher.sink<AddTargetEvent>().connect<&MagicService::OnAddTargetEvent>(this);
     m_notifyAddTargetConnection = m_dispatcher.sink<NotifyAddTarget>().connect<&MagicService::OnNotifyAddTarget>(this);
+    m_removeSpellEventConnection = m_dispatcher.sink<RemoveSpellEvent>().connect<&MagicService::OnRemoveSpellEvent>(this);
+    m_notifyRemoveSpell = m_dispatcher.sink<NotifyRemoveSpell>().connect<&MagicService::OnNotifyRemoveSpell>(this);
 }
 
 void MagicService::OnUpdate(const UpdateEvent& acEvent) noexcept
@@ -415,6 +420,75 @@ void MagicService::OnNotifyAddTarget(const NotifyAddTarget& acMessage) noexcept
 
     spdlog::debug("Applied remote magic effect");
 #endif
+}
+
+void MagicService::OnRemoveSpellEvent(const RemoveSpellEvent& acEvent) noexcept
+{
+    if (!m_transport.IsConnected())
+        return;
+
+    RemoveSpellRequest request{};
+
+    if (!m_world.GetModSystem().GetServerModId(acEvent.SpellId, request.SpellId.ModId, request.SpellId.BaseId))
+    {
+        spdlog::error("{}: Could not find spell with form {:X}", __FUNCTION__, acEvent.SpellId);
+        return;
+    }
+
+    auto view = m_world.view<FormIdComponent>();
+    const auto it = std::find_if(std::begin(view), std::end(view), [id = acEvent.TargetId, view](auto entity) {
+        return view.get<FormIdComponent>(entity).Id == id;
+    });
+
+    if (it == std::end(view))
+    {
+        spdlog::warn("Form id not found for magic remove target, form id: {:X}", acEvent.TargetId);
+        return;
+    }
+
+    std::optional<uint32_t> serverIdRes = Utils::GetServerId(*it);
+    if (!serverIdRes.has_value())
+    {
+        spdlog::warn("Server id not found for magic remove target, form id: {:X}", acEvent.TargetId);
+        return;
+    }
+
+    request.TargetId = serverIdRes.value();
+
+    //spdlog::info("Requesting remove spell with base id {:X} from actor with server id {:X}", request.SpellId.BaseId, request.TargetId);
+
+    m_transport.Send(request);
+}
+
+void MagicService::OnNotifyRemoveSpell(const NotifyRemoveSpell& acMessage) noexcept
+{
+    uint32_t targetFormId = acMessage.TargetId;
+
+    Actor* pActor = Utils::GetByServerId<Actor>(acMessage.TargetId);
+    if (!pActor)
+    {
+        spdlog::warn(__FUNCTION__ ": could not find actor server id {:X}", acMessage.TargetId);
+        return;
+    }
+
+    const uint32_t cSpellId = World::Get().GetModSystem().GetGameId(acMessage.SpellId);
+    if (cSpellId == 0)
+    {
+        spdlog::error("{}: failed to retrieve spell id, GameId base: {:X}, mod: {:X}", __FUNCTION__,
+                      acMessage.SpellId.BaseId, acMessage.SpellId.ModId);
+        return;
+    }
+
+    MagicItem* pSpell = Cast<MagicItem>(TESForm::GetById(cSpellId));
+    if (!pSpell)
+    {
+        spdlog::error("{}: Failed to retrieve spell by id {:X}", __FUNCTION__, cSpellId);
+        return;
+    }
+
+    // Remove the spell from the actor
+    //spdlog::info("Removing spell with form id {:X} from actor with form id {:X}", cSpellId, targetFormId);
+    pActor->RemoveSpell(pSpell);
 }
 
 void MagicService::ApplyQueuedEffects() noexcept

Code/client/Services/MagicService.h
@@ -4,6 +4,7 @@
 #include <Events/EventDispatcher.h>
 #include <Messages/AddTargetRequest.h>
 #include <Messages/NotifyAddTarget.h>
+#include <Messages/NotifyRemoveSpell.h>
 
 struct World;
 struct TransportService;
@@ -12,6 +13,7 @@ struct UpdateEvent;
 struct SpellCastEvent;
 struct InterruptCastEvent;
 struct AddTargetEvent;
+struct RemoveSpellEvent;
 
 struct NotifySpellCast;
 struct NotifyInterruptCast;
@@ -62,6 +64,14 @@ protected:
      * @brief Applies a magic effect based on a server message.
      */
     void OnNotifyAddTarget(const NotifyAddTarget& acMessage) noexcept;
+    /**
+     * @brief Sends a message to remove a spell from a player.
+     */
+    void OnRemoveSpellEvent(const RemoveSpellEvent& acEvent) noexcept;
+    /*
+     * @brief Handles removal of a spell
+     */
+    void OnNotifyRemoveSpell(const NotifyRemoveSpell& acMessage) noexcept;
 
 private:
     /**
@@ -97,4 +107,6 @@ private:
     entt::scoped_connection m_notifyInterruptCastConnection;
     entt::scoped_connection m_addTargetEventConnection;
     entt::scoped_connection m_notifyAddTargetConnection;
+    entt::scoped_connection m_removeSpellEventConnection;
+    entt::scoped_connection m_notifyRemoveSpell;
 };

Code/encoding/Messages/ClientMessageFactory.h
@@ -33,6 +33,7 @@
 #include <Messages/SpellCastRequest.h>
 #include <Messages/InterruptCastRequest.h>
 #include <Messages/AddTargetRequest.h>
+#include <Messages/RemoveSpellRequest.h>
 #include <Messages/ProjectileLaunchRequest.h>
 #include <Messages/ScriptAnimationRequest.h>
 #include <Messages/DrawWeaponRequest.h>
@@ -68,7 +69,7 @@ struct ClientMessageFactory
             PartyChangeLeaderRequest, PartyKickRequest, RequestActorValueChanges, RequestActorMaxValueChanges, EnterExteriorCellRequest, RequestHealthChangeBroadcast, ActivateRequest, LockChangeRequest, AssignObjectsRequest, RequestDeathStateChange, ShiftGridCellRequest,
             RequestOwnershipTransfer, RequestOwnershipClaim, RequestObjectInventoryChanges, SpellCastRequest, ProjectileLaunchRequest, InterruptCastRequest, AddTargetRequest, ScriptAnimationRequest, DrawWeaponRequest, MountRequest, NewPackageRequest, RequestRespawn, SyncExperienceRequest,
             RequestEquipmentChanges, SendChatMessageRequest, TeleportCommandRequest, PlayerRespawnRequest, DialogueRequest, SubtitleRequest, PlayerDialogueRequest, PlayerLevelRequest, TeleportRequest, RequestPlayerHealthUpdate, RequestWeatherChange, RequestCurrentWeather, RequestSetWaypoint,
-            RequestRemoveWaypoint>;
+            RequestRemoveWaypoint, RemoveSpellRequest>;
 
         return s_visitor(std::forward<T>(func));
     }

Code/encoding/Messages/NotifyRemoveSpell.cpp
@@ -1,0 +1,13 @@
+#include "NotifyRemoveSpell.h"
+
+void NotifyRemoveSpell::SerializeRaw(TiltedPhoques::Buffer::Writer& aWriter) const noexcept
+{
+    Serialization::WriteVarInt(aWriter, TargetId);
+    SpellId.Serialize(aWriter);
+}
+
+void NotifyRemoveSpell::DeserializeRaw(TiltedPhoques::Buffer::Reader& aReader) noexcept
+{
+    TargetId = Serialization::ReadVarInt(aReader);
+    SpellId.Deserialize(aReader);
+}

commit 86cfa60268322763c56e0d0a591405d95bf684d7

Fix FT build broken by c47c3bef09540106d71db22d427b8aa57c818ecf

Code/client/Games/Fallout4/Actor.h
@@ -145,6 +145,7 @@ struct Actor : TESObjectREFR
     void SetCombatTargetEx(Actor* apTarget) noexcept;
     void StartCombat(Actor* apTarget) noexcept;
     void StopCombat() noexcept;
+    void RemoveSpell(MagicItem* apSpell) noexcept;
 
     enum ActorFlags
     {

Code/client/Games/References.cpp
@@ -857,9 +857,9 @@ void Actor::StopCombat() noexcept
     s_pStopCombat(this);
 }
 
-#if TP_SKYRIM64
 void Actor::RemoveSpell(MagicItem* apSpell) noexcept
 {
+#if TP_SKYRIM64
     TP_THIS_FUNCTION(TRemoveSpell, void, Actor, MagicItem*);
     POINTER_SKYRIMSE(TRemoveSpell, removeSpell, 38717);
     if (!apSpell)
@@ -869,8 +869,10 @@ void Actor::RemoveSpell(MagicItem* apSpell) noexcept
     }
     //spdlog::info("Removing spell: {} from actor: {}", apSpell->formID, formID);
     TiltedPhoques::ThisCall(removeSpell, this, apSpell);
-}
+#elif TP_FALLOUT4
+    // TODO ft, does it even have a RemoveSpell?
 #endif
+}
 bool Actor::HasPerk(uint32_t aPerkFormId) const noexcept
 {
     return GetPerkRank(aPerkFormId) != 0;