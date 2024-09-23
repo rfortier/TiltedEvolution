5c120d1dca39f5d5c1231d546639de0fe9125f50
rfortier <rwf@ourchateau.net>
2024-09-23 19:50:16 -0400

    Repair Nvidia black-screen fix to reenable ENB mod loading.
    The ENB mod places a proxy DLL in the SkyrimSE.exe directory to hook the D3D11 functions it needs. But SkyrimTogether.exe launches from a different directory, so the default DLL search path doesn't find it. Check for existence of the DLL and load it if found.
    
    Also fixes a minor invocation bug.

--- Code/client/NvidiaUtil.cpp
+++ Code/client/NvidiaUtil.cpp
@@ -1,4 +1,5 @@
 #include "NvidiaUtil.h"
+#include "immersive_launcher/launcher.h"
 #include <d3d11.h>
 
 bool IsNvidiaOverlayLoaded()
@@ -9,8 +10,20 @@ bool IsNvidiaOverlayLoaded()
 // This makes the Nvidia overlay happy.
 // The call to D3D11CreateDevice probably causes some of their
 // internal hooks to be called and do the required init work before the game window opens.
-HRESULT CreateEarlyDxDevice(ID3D11Device* apOutDevice, D3D_FEATURE_LEVEL* apOutFeatureLevel)
+HRESULT CreateEarlyDxDevice(ID3D11Device** appOutDevice, D3D_FEATURE_LEVEL* apOutFeatureLevel)
 {
-    return D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &apOutDevice,
-                             apOutFeatureLevel, nullptr);
+    // ENB (https://enbdev.org) works by injecting a proxy d3d11.dll into the SkyrimSE.exe
+    // directory. It won't be loaded if SkyrimTogether.exe compile-time links to d3d11.dll, 
+    // as it is in a different directory. To fix, launch ENB version if it is there, otherwise
+    // launch via the standard search path.
+    auto LC = launcher::GetLaunchContext();
+    auto d3d11Path = LC->gamePath / "d3d11.dll";
+    auto pModule = LoadLibraryExW(d3d11Path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+
+    // If that failed, standard lookup.
+    if (!pModule)
+        pModule = LoadLibraryExW(d3d11Path.filename().c_str(), nullptr, 0);
+    const auto pD3D11CreateDevice = reinterpret_cast<decltype(&D3D11CreateDevice)>(GetProcAddress(pModule, "D3D11CreateDevice"));
+
+    return pD3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, appOutDevice, apOutFeatureLevel, nullptr);
 }
